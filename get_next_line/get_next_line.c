ecb417c07f2cab61a51ec61f4bc2bcabe232e4ea
Hamza Benmohamd <hbenmoha@e2r2p11.1337.ma>
2025-04-10 17:06:55 +0100

    update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,126 +1,87 @@
-#include <stdlib.h>
-#include <unistd.h>
-
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+#	define BUFFER_SIZE 42
 #endif
 
-static int	ft_strlen(char *s)
+#include <unistd.h>
+#include <stdlib.h>
+
+char *str_chr (char *s, int c)
+{
+	while (*s) {
+		if (*s == (char)c)
+			return((char *)s);
+		s++;
+	}
+	return (NULL);
+}
+
+int ft_strlen(char *s)
 {
 	int i = 0;
-	while (s && s[i])
+	while (s[i])
 		i++;
 	return (i);
 }
 
-static char	*ft_strchr(char *s, int c)
+void  str_copy(char *s, char* d)
 {
-	int i = 0;
-	if (!s)
-		return (0);
-	while (s[i])
-	{
-		if (s[i] == (char)c)
-			return (&s[i]);
-		i++;
-	}
-	if (c == 0)
-		return (&s[i]);
-	return (0);
+	while (*d)
+		*s++ = *d++;
+	*s = '\0';
 }
 
-static char	*ft_strjoin(char *s1, char *s2)
+char *ft_strdup(char *s)
 {
-	int i = 0, j = 0;
-	char *new = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!new)
-		return (0);
-	while (s1 && s1[i])
-	{
-		new[i] = s1[i];
-		i++;
-	}
-	while (s2 && s2[j])
-		new[i++] = s2[j++];
-	new[i] = 0;
-	free(s1);
-	return (new);
+	char *c = malloc(ft_strlen(s) + 1);
+	str_copy(c, s);
+	return(c);
 }
 
-static char	*get_line(char *stash)
+char *ft_strjoin(char *s, char *d)
 {
-	int i = 0;
-	char *line;
-
-	if (!stash || !stash[0])
-		return (0);
-	while (stash[i] && stash[i] != '\n')
-		i++;
-	if (stash[i] == '\n')
-		i++;
-	line = malloc(i + 1);
-	if (!line)
-		return (0);
-	i = 0;
-	while (stash[i] && stash[i] != '\n')
-	{
-		line[i] = stash[i];
-		i++;
-	}
-	if (stash[i] == '\n')
-		line[i++] = '\n';
-	line[i] = 0;
-	return (line);
+	if (!s || !d)
+		return (NULL);
+	char *ss = malloc(ft_strlen(s) + ft_strlen(d) + 1);
+	if (!ss)
+		return (NULL);
+	str_copy(ss ,s);
+	str_copy(ss + ft_strlen(s) ,d);
+	free(s);
+	return(ss);
 }
 
-static char	*get_rest(char *stash)
+char *get_next_line(int fd)
 {
-	int i = 0, j = 0;
-	char *rest;
+	static char buf[BUFFER_SIZE + 1]; //? A static buffer stores leftover content between calls.
+	char *line; //? the line that will be returned
+	char *new_line;
+	int i;
+	int j;
 
-	while (stash[i] && stash[i] != '\n')
-		i++;
-	if (!stash[i])
+	line = ft_strdup(buf);
+
+	//? read from the fd and append to the last str until we found a '\n' 
+	while (!(new_line = str_chr(line, '\n')) && (i = read(fd, buf, BUFFER_SIZE))) 
 	{
-		free(stash);
-		return (0);
+		buf[i] = '\0';
+		line = ft_strjoin(line, buf);
 	}
-	rest = malloc(ft_strlen(stash) - i + 1);
-	if (!rest)
-		return (0);
-	i++;
-	while (stash[i])
-		rest[j++] = stash[i++];
-	rest[j] = 0;
-	free(stash);
-	return (rest);
-}
 
-char	*get_next_line(int fd)
-{
-	static char *stash;
-	char buffer[BUFFER_SIZE + 1];
-	int bytes;
-	char *line;
+	//? if read return 0 ( i = 0 ) which mean we reach the EOF (for example in an empty line)
+	if (ft_strlen(line) == 0)
+		return(free(line), NULL);
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
-	bytes = 1;
-	while (!ft_strchr(stash, '\n') && bytes > 0)
+	if (new_line != NULL) //? save characters after '\n' in a the buf (if we foun the '\n')
 	{
-		bytes = read(fd, buffer, BUFFER_SIZE);
-		if (bytes < 0)
-		{
-			free(stash);
-			stash = 0;
-			return (0);
-		}
-		buffer[bytes] = 0;
-		stash = ft_strjoin(stash, buffer);
+		j = new_line - line + 1; //* j = 11 - 0 + 1 = 12 ( new_line = ptr to '\n') ( line ptr to the first char of str) ( + 1 of the '\n' => j = indix after '\n' )
+		str_copy(buf, new_line + 1); //* copy chars that are after '\n' in the buf ( daba lbufer fih cheyata )
 	}
-	line = get_line(stash);
-	stash = get_rest(stash);
-	return (line);
-}
+	else //? when we found the last line in the fd whitout a '\n'.
+	{  //? we calculate it's lenght and null terminate the buffer and we null terminate the last line . then we return it
+		j = ft_strlen(line);
+		buf[0] = '\0';
+	}
 
-//! pased in the gradmeme test!
+	line[j] = '\0'; //* j = indix after '\n'
+	return(line);
+}
